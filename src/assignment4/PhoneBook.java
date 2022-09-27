cd13326f2b2d7fe9f6b0ab54c8fd328e4f6769c3
vietdung96 <102815340+vietdung96@users.noreply.github.com>
2022-09-27 17:56:51 +0700

    bai tap 4

---
+++ src/assignment4/PhoneBook.java
@@ -0,0 +1,34 @@
+package assignment4;
+
+import java.util.ArrayList;
+
+public class PhoneBook extends Phone{
+    public ArrayList<PhoneNumber> PhoneList;
+
+    @Override
+    public void insertPhone(String name, String phone) {
+
+
+
+    }
+
+    @Override
+    public void removePhone(String name) {
+
+    }
+
+    @Override
+    public void updatePhone(String name, String oldPhone, String newPhone) {
+
+    }
+
+    @Override
+    public void searchPhone(String name) {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
---
+++ src/assignment4/PhoneNumber.java
@@ -0,0 +1,8 @@
+package assignment4;
+
+import java.util.ArrayList;
+
+public class PhoneNumber {
+    String name;
+    ArrayList<String> phone;
+}
